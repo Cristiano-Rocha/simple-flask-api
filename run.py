d4eeb1d66df328e9fea41e6cc4e1af4a0a56fcb3
Cristiano-Rocha <sdxadev@gmail.com>
2022-09-23 23:49:01 -0300

    Remove host definition from app.run

--- run.py
+++ run.py
@@ -4,14 +4,13 @@ from flask_migrate import Migrate
 
 app = create_app()
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {
-        'db': db,
-        'Dog': Dog
-    }
+    return {"db": db, "Dog": Dog}
+
 
 migrate = Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7001, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
